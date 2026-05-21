cebf1d9ee9955e28bf26a5becbc08a93d0349894
Amanda <simangeleramavhale@gmail.com>
2026-05-21 17:32:20 +0200

    added dependancies, home,login page loxators and methods

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,5 +1,35 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
 public class HomePage {
+
+    WebDriver driver;
+
+    @FindBy(xpath = "//h3[text()='Selenium, Cypress, Playwright']")
+    WebElement WebAutomationTools;
+
+    @FindBy(xpath = "//button[@class='user-pill']")
+    WebElement loginButton;
+
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+
+    }
+
+    public String getWelcomeMessage(){
+        return WebAutomationTools.getText();
+    }
+
+    public void clickLoginButton(){
+        loginButton.click();
+    }
 
 }
--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,31 @@
 package Pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+    @FindBy (xpath = "//span[contains(.,'Access Learning Materials')]")
+    WebElement accessLearningMaterialsLabel;
+
+    @FindBy(id="login-email")
+    WebElement usernameField;
+
+    @FindBy(id="login-password")
+    WebElement passwordField;
+
+    @FindBy(id="login-submit")
+    WebElement loginButton;
+
+    public String getAccessLearningMaterialsLabelText(){
+        return accessLearningMaterialsLabel.getText();
+    }
+
+    public void enterUsername(String username){
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password){
+        passwordField.sendKeys(password);
+    }
 }
